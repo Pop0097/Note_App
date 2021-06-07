ae6887e43c72cdf717148fe9c01d1c47c49d7a09
Pop0097 <dhruvrawat09@gmail.com>
2021-06-07 15:41:19 -0400

    uhh

--- Pods/Target Support Files/Pods-awsApp/Pods-awsApp-frameworks.sh
+++ Pods/Target Support Files/Pods-awsApp/Pods-awsApp-frameworks.sh
@@ -181,8 +181,11 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AWSCore/AWSCore.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AWSMobileClient/AWSMobileClient.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AWSPluginsCore/AWSPluginsCore.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSS3/AWSS3.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Amplify/Amplify.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AmplifyPlugins/AmplifyPlugins.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AppSyncRealTimeClient/AppSyncRealTimeClient.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Starscream/Starscream.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AWSAuthCore/AWSAuthCore.framework"
@@ -191,8 +194,11 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AWSCore/AWSCore.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AWSMobileClient/AWSMobileClient.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AWSPluginsCore/AWSPluginsCore.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSS3/AWSS3.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Amplify/Amplify.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AmplifyPlugins/AmplifyPlugins.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AppSyncRealTimeClient/AppSyncRealTimeClient.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Starscream/Starscream.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
--- awsApp/ContentView.swift
+++ awsApp/ContentView.swift
@@ -25,11 +25,33 @@ class Note : Identifiable, ObservableObject {
     var imageName : String?
     @Published var image : Image?
     
+    fileprivate var _data : NoteData? // Scope is only this file. Like a static global variable in C
+    
     init(id: String, name: String, description: String? = nil /* Optional parameter */, image: String? = nil) {
         self.id = id
         self.name = name
         self.description = description
         self.imageName = image
+    }
+    
+    convenience init(from data: NoteData) {
+        self.init(id: data.id, name: data.name, description: data.description, image: data.image)
+        
+        // Store API object
+        self._data = data
+    }
+
+    // access the privately stored NoteData or build one if we don't have one.
+    var data : NoteData {
+
+        if (_data == nil) {
+            _data = NoteData(id: self.id,
+                            name: self.name,
+                            description: self.description,
+                            image: self.imageName)
+        }
+
+        return _data!
     }
 }
 
@@ -88,9 +110,61 @@ struct SignOutButton : View {
     }
 }
 
+struct AddNoteView: View {
+    // @Binding lets us declare that one value actually comes from elsewhere, and should be shared in both places.
+    @Binding var isPresented: Bool
+    
+    var userData: UserData
+
+    @State var name : String = "New Note"
+    @State var description : String = "This is a new note"
+    @State var image : String = "image"
+    
+    var body: some View {
+        Form {
+            Section(header: Text("TEXT")) {
+                TextField("Name", text: $name)
+                TextField("Name", text: $description)
+            }
+
+            Section(header: Text("PICTURE")) {
+                TextField("Name", text: $image)
+            }
+
+            Section {
+                Button(action: {
+                    self.isPresented = false
+                    
+                    // Creates new NoteData object
+                    let noteData = NoteData(id : UUID().uuidString,
+                                            name: self.$name.wrappedValue,
+                                            description: self.$description.wrappedValue)
+                    
+                    let note = Note(from: noteData) // Makes note
+
+                    // asynchronously store the note (and assume it will succeed)
+                    Backend.shared.createNote(note: note)
+
+                    // add the new note in our userdata, this will refresh UI
+                    self.userData.notes.append(note)
+                }) {
+                    Text("Create this note")
+                }
+            }
+        }
+    }
+}
+
 // Main view of the Application. What loads first
 struct ContentView: View {
+    
     @ObservedObject private var userData : UserData = .shared
+    
+    // When the state value changes, the view invalidates its appearance and recomputes the body.
+    @State var showCreateNote = false
+    @State var name : String = "New Note"
+    @State var description : String = "This is a new note"
+    @State var image : String = "image"
     
     var body: some View {
         ZStack {
@@ -99,10 +173,24 @@ struct ContentView: View {
                     List {
                         ForEach(userData.notes) { note in // Other form of a for loop. Same as "for note in userData.notes"
                             ListRow(note: note)
+                        }.onDelete { indices in
+                            indices.forEach {
+                                // removing from user data will refresh UI
+                                let note = self.userData.notes.remove(at: $0)
+
+                                // asynchronously remove from database
+                                Backend.shared.deleteNote(note: note)
+                            }
                         }
                     }
                     .navigationBarTitle(Text("Notes"))
-                    .navigationBarItems(leading: SignOutButton())
+                    .navigationBarItems(leading: SignOutButton(), trailing: Button(action: {
+                        self.showCreateNote.toggle() // Toggles this event (just below)
+                    }) {
+                        Image(systemName: "plus")
+                    })
+                }.sheet(isPresented: $showCreateNote /* Listener event */) {
+                    AddNoteView(isPresented: self.$showCreateNote, userData: self.userData)
                 }
             } else {
                 SignInButton()
